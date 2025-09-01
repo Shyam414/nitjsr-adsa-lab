e82a06c4429274af1a3d7e822f1c751c1a2f16e3
Shyam414 <sundarreddy414@gmail.com>
2025-09-01 17:06:08 +0530

    commit

---
+++ set_03_07_01Que2stack.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack1[MAX], stack2[MAX];
+int top1 = -1, top2 = -1;
+
+// Stack operations
+void push1(int x) { stack1[++top1] = x; }
+int pop1() { return stack1[top1--]; }
+void push2(int x) { stack2[++top2] = x; }
+int pop2() { return stack2[top2--]; }
+int isEmpty1() { return top1 == -1; }
+int isEmpty2() { return top2 == -1; }
+
+// Method 1: Push costly
+void enqueue(int x) {
+    while (!isEmpty1()) {
+        push2(pop1());
+    }
+    // Push new element to stack1
+    push1(x);
+    // Move everything back to stack1
+    while (!isEmpty2()) {
+        push1(pop2());
+    }
+}
+
+int dequeue() {
+    if (isEmpty1()) {
+        printf("Queue Underflow!\n");
+        return -1;
+    }
+    return pop1();
+}
+
+int peek() {
+    if (isEmpty1()) {
+        printf("Queue Empty!\n");
+        return -1;
+    }
+    return stack1[top1];
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    printf("Dequeued: %d\n", dequeue());
+    enqueue(40);
+    printf("Dequeued: %d\n", dequeue()); 
+    printf("Peek: %d\n", peek());        
+    return 0;
+}
---
+++ set_03_07_02Que2stack.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack1[MAX], stack2[MAX];
+int top1 = -1, top2 = -1;
+
+// Stack operations
+void push1(int x) { stack1[++top1] = x; }
+int pop1() { return stack1[top1--]; }
+void push2(int x) { stack2[++top2] = x; }
+int pop2() { return stack2[top2--]; }
+int isEmpty1() { return top1 == -1; }
+int isEmpty2() { return top2 == -1; }
+
+// Method 2: Pop costly
+void enqueue(int x) {
+    push1(x); // simple push
+}
+
+int dequeue() {
+    if (isEmpty1()) {
+        printf("Queue Underflow!\n");
+        return -1;
+    }
+    // Move everything except last to stack2
+    while (top1 > 0) {
+        push2(pop1());
+    }
+    int front = pop1(); // dequeue element
+    // Move back to stack1
+    while (!isEmpty2()) {
+        push1(pop2());
+    }
+    return front;
+}
+
+int peek() {
+    if (isEmpty1()) {
+        printf("Queue Empty!\n");
+        return -1;
+    }
+    while (top1 > 0) {
+        push2(pop1());
+    }
+    int front = stack1[top1]; 
+    while (!isEmpty2()) {
+        push1(pop2());
+    }
+    return front;
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    printf("Dequeued: %d\n", dequeue());
+    enqueue(40);
+    printf("Dequeued: %d\n", dequeue()); 
+    printf("Peek: %d\n", peek());       
+    return 0;
+}
