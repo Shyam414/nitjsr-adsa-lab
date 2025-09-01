924559faa0c08e11ed2f3c103a4015fb9795a5fd
Shyam414 <sundarreddy414@gmail.com>
2025-09-01 16:51:56 +0530

    commit

--- set_03_06_02circularQueuewithLinkedList.c
+++ set_03_06_02circularQueuewithLinkedList.c
@@ -64,3 +64,35 @@ void display(){
     }while(temp!=front);
     printf("\n");
 }
+
+int main(){
+    int choice, value;
+    while(1){
+        printf("1. Enqueue\n2. Dequeue\n3. Peek\n4. Display\n5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                printf("Enter value to enqueue: ");
+                scanf("%d",&value);
+                enqueue(value);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                value=peek();
+                if(value!=-1)
+                    printf("Front element: %d\n",value);
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+    return 0;
+}
