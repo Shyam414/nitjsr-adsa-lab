60776057303a384ca3ce0bd4d9c9a50ffd0f8977
Shyam414 <sundarreddy414@gmail.com>
2025-09-01 12:47:44 +0530

    commit

---
+++ set-02/MergeSortworecursion.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+// Merge two sorted subarrays
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1, n2 = r - m;
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+// Bottom-Up merge sort
+void mergeSortBottomUp(int arr[], int n) {
+    for (int curr_size = 1; curr_size < n; curr_size *= 2) {
+        for (int left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
+            int mid = left_start + curr_size - 1;
+            int right_end = (left_start + 2 * curr_size - 1 < n - 1) ? left_start + 2 * curr_size - 1 : n - 1;
+
+            if (mid < right_end)
+                merge(arr, left_start, mid, right_end);
+        }
+    }
+}
+
+// Print array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array before sorting: ");
+    printArray(arr, n);
+
+    mergeSortBottomUp(arr, n);
+
+    printf("Array after sorting: ");
+    printArray(arr, n);
+
+    return 0;
+}
